dcbb97f048ad241b074adbe015e105f8140f3e74
idan asulin <djidanasulin@gmail.com>
2020-01-31 10:58:50 +0200

    establish db connection & add basic web server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 require('./dbConnection');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
