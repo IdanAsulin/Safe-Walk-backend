a3239806ad191c99e3e9e96d8d14aa1e5f5652dd
idan asulin <djidanasulin@gmail.com>
2020-02-01 18:21:55 +0200

    delete morgan logging

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Joi = require('joi');
-const morgan = require('morgan');
 require('./dbConnection');
 
 const app = express();
@@ -8,6 +7,5 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan(':method :url :status :response-time ms'));
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
