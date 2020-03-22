ee8b1a1aab72cc71d45d3fd7a2d7cda46afa6fb7
idan asulin <djidanasulin@gmail.com>
2020-03-22 15:53:47 +0200

    test

--- middlewares.js
+++ middlewares.js
@@ -32,7 +32,7 @@ module.exports = {
 
     blockNotLambda(req, res, next) {
         const token = req.headers['x-auth-token'] || '';
-        console.log(`\n\nreq: ${req.headers['x-auth-token']}`);
+        console.log(`\n\nreq: ${JSON.stringify(req.headers)}`);
         if (!token) {
             logger.warn(`User did not send token`);
             return res.status(401).json({
