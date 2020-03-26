58560661b64584fedb021ecd063de02767ffdb60
idan asulin <djidanasulin@gmail.com>
2020-03-26 20:11:19 +0200

    update AWS lambda code

--- AWS Lambda Functions/Complementary Filter/index.js
+++ AWS Lambda Functions/Complementary Filter/index.js
@@ -1,7 +1,7 @@
 const request = require('request-promise');
 
 const serverURL = "http://ec2-3-89-190-108.compute-1.amazonaws.com:3000/api";
-const LAMBDA_SECRET = '<SECRET LAMBDA KEY>';
+const LAMBDA_SECRET = 'YanIv_!2#4IdaN__--AvI';
 
 exports.handler = async (event, context, callback) => {
     const timeDifference = 1 / event.SAMPLE_RATE_HZ;
@@ -9,28 +9,30 @@ exports.handler = async (event, context, callback) => {
     const lpf = event.LOW_PASS_FILTER;
     // const degreesToRadians = Math.PI / 180;
     const radiansToDegrees = 180 / Math.PI;
-    let roll_angle_x = 0;
-    let pitch_angle_y = 0;
+    let pitch_angle_x = 0;
+    let roll_angle_y = 0;
     let yaw_angle_z = 0;
     const anglesArray = [];
     for (let raw of event.rawData) {
-        const acc_roll_angle = Math.atan2(raw.zA, raw.yA) * radiansToDegrees;
-        const acc_pitch_angle = Math.atan2(raw.xA, raw.zA) * radiansToDegrees;
-        const acc_yaw_angle = Math.atan2(raw.yA, raw.xA) * radiansToDegrees;
-        roll_angle_x = hpf * roll_angle_x + raw.xG * timeDifference + lpf * acc_roll_angle;
-        pitch_angle_y = hpf * pitch_angle_y + raw.yG * timeDifference + lpf * acc_pitch_angle;
-        yaw_angle_z = hpf * yaw_angle_z + raw.zG * timeDifference + lpf * acc_yaw_angle;
+        const accAnglePitch = Math.atan2(raw.xA, Math.sqrt((raw.yA * raw.yA) + (raw.zA * raw.zA))) * radiansToDegrees;
+        const accAngleRoll = Math.atan2(raw.yA, Math.sqrt((raw.xA * raw.xA) + (raw.zA * raw.zA))) * radiansToDegrees;
+        const accAngleYaw = Math.atan2(raw.zA, Math.sqrt((raw.xA * raw.xA) + (raw.yA * raw.yA))) * radiansToDegrees;
+        const gyroAnglePitch = pitch_angle_x + (raw.yG * timeDifference);
+        const gyroAngleRoll = roll_angle_y + (raw.xG * timeDifference);
+        const gyroAngleYaw = yaw_angle_z + (raw.xG * timeDifference);
+        pitch_angle_x = ((hpf * gyroAnglePitch) + (lpf * accAnglePitch));
+        roll_angle_y = ((hpf * gyroAngleRoll) + (lpf * accAngleRoll));
+        yaw_angle_z = ((hpf * gyroAngleYaw) + (lpf * accAngleYaw));
         anglesArray.push({
             timeStamp: raw.t,
-            roll_angle_x: roll_angle_x,
-            pitch_angle_y: pitch_angle_y,
+            pitch_angle_x: pitch_angle_x,
+            roll_angle_y: roll_angle_y,
             yaw_angle_z: yaw_angle_z
         });
     }
-    anglesArray.splice(0,150); // delete the first 150 samples for sensor fusion needs 
     const options = {
         url: `${serverURL}/patientGaitModel/${event.testID}`,
-        headers: { 
+        headers: {
             'Content-Type': 'application/json',
             'x-auth-token': LAMBDA_SECRET
         },
