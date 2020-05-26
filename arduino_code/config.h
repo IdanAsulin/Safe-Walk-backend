18241b7c39beb8905548cf3c94faeebb76c3cab8
idan asulin <djidanasulin@gmail.com>
2020-05-26 21:43:40 +0300

    fix

--- arduino_code/config.h
+++ arduino_code/config.h
@@ -1,5 +1,5 @@
-#define WIFI_NAME "WIFI NETWORK NMAE"
-#define WIFI_PASS "WIFI NETWORK PASSWORD"
+#define WIFI_NAME "Idanasulin"
+#define WIFI_PASS "0523858877"
 #define KIT_ID "24939e89-7aef-4d3d-978b-73b63fd4e7d2"
 #define SENSOR_NAME "sensor1"
-#define SAMPLE_TIME 15000
+#define SAMPLE_TIME 20000
