510da68d48f90a27746417d55cc7b6c19a4192b4
idan asulin <djidanasulin@gmail.com>
2020-05-27 00:41:04 +0300

    add https support

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('./dbConnection');
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
 const morgan = require('morgan');
 const { validateRequestBody } = require('./middlewares');
 const logger = require('./logger');
-const app = express();
-const port = process.env.PORT || 3000;
+const config = require('./config.json');
+const fs = require('fs');
 
+const developmentPort = 3000;
+const productionPort = 443;
+
+const app = express();
 app.use(express.json({ limit: '15mb' }));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -24,4 +30,13 @@ app.use('/api/video', require('./routes/video'));
 app.use('/api/auth', require('./routes/auth'));
 app.all('*', (req, res) => res.status(404).json({ message: `Endpoint not found` }));
 
-app.listen(port, () => logger.info(`Listening on port: ${port}`));
+if (process.env.NODE_ENVIRONMENT === 'production') {
+    const options = {
+        key: fs.readFileSync(config.SSL_KEY_PATH),
+        cert: fs.readFileSync(config.SSL_CERT_PATH)
+    };
+    const httpsServer = https.createServer(options, app);
+    httpsServer.listen(productionPort, () => console.log(`Listening on port ${productionPort}`))
+}
+else
+    app.listen(developmentPort, () => logger.info(`Listening on port: ${developmentPort}`));
